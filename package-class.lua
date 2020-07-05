755de9f6ef26d08687b75368c4f3faed9930bffc
neverclear <neverclear86sab@gmail.com>
2020-07-06 06:11:49 +0900

    なんかダメだった

--- package-class.lua
+++ package-class.lua
@@ -2,52 +2,51 @@ local lon = require "lon"
 local instance = require "class"
 
 --- パッケージ操作クラス
-local Package = {
+local Package = {}
 
-    new = function()
-        local obj = instance(Package)
+function Package.new()
+    local obj = instance(self)
 
-        obj.author = ""
-        obj.name = ""
-        obj.description = ""
-        obj.repository = ""
-        obj.version = ""
-        obj.files = {"init.lua",}
-        obj.dependencies = {}
+    obj.author = ""
+    obj.name = ""
+    obj.description = ""
+    obj.repository = ""
+    obj.version = ""
+    obj.files = {"init.lua",}
+    obj.dependencies = {}
 
-        return obj
-    end,
+    return obj
+end
 
-    isExistsDependencies = function(self, packageName)
-        for i, v in pairs(self.dependencies) do
-            if v == packageName then
-                return true
-            end
+function Package:isExistsDependencies(packageName)
+    for i, v in pairs(self.dependencies) do
+        if v == packageName then
+            return true
         end
-        return false
-    end,
+    end
+    return false
+end
 
-    addDependencies = function(self, packageName)
-        table.insert(self.dependencies, packageName)
-    end,
+function Package:addDependencies(packageName)
+    table.insert(self.dependencies, packageName)
+end
 
-    removeDependencies = function(self, packageName)
-        for i, v in ipairs(self.dependencies) do
-            if packageName == v then
-                table.remove(self.dependencies, i)
-                return
-            end
+function Package:removeDependencies(packageName)
+    for i, v in ipairs(self.dependencies) do
+        if packageName == v then
+            table.remove(self.dependencies, i)
+            return
         end
-    end,
+    end
+end
 
-    save = function(self, file)
-        lon.save(file, self)
-    end,
+function Package:save(file)
+    lon.save(file, self)
+end
 
-    Load = function(file)
-        lon.load(file)
-    end,
-}
+function Package.Load(file)
+    lon.load(file)
+end
 
 
 return Package
